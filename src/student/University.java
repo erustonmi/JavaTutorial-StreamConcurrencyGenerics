73c0894b0ef92ac0ae5ff03419ac32715f325bb6
SimonHGR <simon@dancingcloudservices.com>
2016-10-10 15:51:38 -0400

    Genericized criterioin filter

--- src/student/University.java
+++ src/student/University.java
@@ -1,48 +1,53 @@
 package student;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-interface StudentCriterion {
-	boolean test(Student s);
+interface Criterion<E> {
+	boolean test(E e);
 }
 
 public class University {
 
-	public static List<Student> getStudentsByCriterion(Iterable<Student> input, StudentCriterion criterion) {
-		List<Student> rv = new ArrayList<>();
-		for (Student s : input) {
+	public static <E> List<E> getByCriterion(Iterable<E> input, Criterion<E> criterion) {
+		List<E> rv = new ArrayList<>();
+		for (E s : input) {
 			if (criterion.test(s)) {
 				rv.add(s);
 			}
 		}
 		return rv;
 	}
-	
+
 	public static void main(String[] args) {
 		Set<Student> roster = new HashSet<>();
 		roster.add(Student.fromNameGpaCourseList("Fred", 2.8F, "Math", "Physics"));
-		roster.add(Student.fromNameGpaCourseList("Sheila", 3.8F, new String[] {"Biology", "Physics", "Bio-chemistry"}));
+		roster.add(
+				Student.fromNameGpaCourseList("Sheila", 3.8F, new String[] { "Biology", "Physics", "Bio-chemistry" }));
 		roster.add(Student.fromNameGpaCourseList("Jim", 3.9F, "Art"));
 		roster.add(Student.fromNameGpaCourseList("Fred", 2.8F, "Math", "Physics"));
-	
+
 		for (Student s : roster) {
 			System.out.println(s);
 		}
-		
+
 		List<Student> lRoster = new ArrayList<>(roster);
-		
+
 		System.out.println("Before:");
 		System.out.println(lRoster);
 		lRoster.sort(Student.getGpaComparator());
 		System.out.println("After:");
 		System.out.println(lRoster);
-		
+
 		System.out.println("-----------");
-		System.out.println("Smart students" + getStudentsByCriterion(roster, (s)-> s.getGpa() > 3.0F));
-		
+		System.out.println("Smart students" + getByCriterion(roster, (s) -> s.getGpa() > 3.0F));
+
+		System.out.println(
+				"> " + getByCriterion(Arrays.asList("Banana", "Orange", "Mango", "Kiwi"), (s) -> s.length() > 5));
+
 	}
 
 }
