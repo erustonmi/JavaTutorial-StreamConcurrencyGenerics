7be70f718f9f8491545ee930d9a36c7924c03cc9
SimonHGR <simon@dancingcloudservices.com>
2016-10-10 16:37:03 -0400

    Added generit "andThenComparing" method to combine comparators

--- src/student/University.java
+++ src/student/University.java
@@ -2,6 +2,7 @@ package student;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,6 +10,17 @@ import java.util.function.Predicate;
 
 public class University {
 
+	public static <E> Comparator<E> andThenCompare(Comparator<E> first, Comparator<E> second) {
+		return (a,b) -> {
+			int t1 = first.compare(a, b);
+			if (t1 == 0) {
+				return second.compare(a, b);
+			} else {
+				return t1;
+			}
+		};
+	}
+	
 	public static <E> List<E> getByCriterion(Iterable<E> input, Predicate<E> criterion) {
 		List<E> rv = new ArrayList<>();
 		for (E s : input) {
@@ -19,9 +31,19 @@ public class University {
 		return rv;
 	}
 
+	public static void showIterable(Iterable i) {
+		for (Object o : i) {
+			System.out.println(o);
+		}
+	}
+	
 	public static void main(String[] args) {
 		Set<Student> roster = new HashSet<>();
 		roster.add(Student.fromNameGpaCourseList("Fred", 2.8F, "Math", "Physics"));
+		roster.add(Student.fromNameGpaCourseList("Alan", 2.8F, "Math", "Physics", "Chemistry"));
+		roster.add(Student.fromNameGpaCourseList("Sarah", 3.0F, "Math", "Physics"));
+		roster.add(Student.fromNameGpaCourseList("Alice", 3.0F, "Math", "Physics", "Biology"));
+		roster.add(Student.fromNameGpaCourseList("Denise", 3.0F, "Math", "Physics","Biology", "Organic Chemistry"));
 		roster.add(
 				Student.fromNameGpaCourseList("Sheila", 3.8F, new String[] { "Biology", "Physics", "Bio-chemistry" }));
 		roster.add(Student.fromNameGpaCourseList("Jim", 3.9F, "Art"));
@@ -45,6 +67,13 @@ public class University {
 		System.out.println(
 				"> " + getByCriterion(Arrays.asList("Banana", "Orange", "Mango", "Kiwi"), (s) -> s.length() > 5));
 
+		System.out.println("-----------");
+		Comparator<Student> gpa = Student.getGpaComparator();
+		Comparator<Student> courseCount = (a,b) -> a.getCourses().size() - b.getCourses().size();
+		Comparator<Student> combined = andThenCompare(gpa, courseCount);
+		lRoster.sort(combined);
+		showIterable(lRoster);
+		
 	}
 
 }
