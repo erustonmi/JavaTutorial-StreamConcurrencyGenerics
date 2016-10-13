0c442ee97f3451d8f1dc9440a059fe4c1112e835
SimonHGR <simon@dancingcloudservices.com>
2016-10-13 15:51:14 -0400

    Added broken sharing example, and sound sharing using Blocking Queue

---
+++ src/producerconsumer/BrokenSharing.java
@@ -0,0 +1,47 @@
+package producerconsumer;
+
+public class BrokenSharing {
+	private int a, b;
+	
+	public void increment() {
+		a++;
+		if (a >= 10) {
+			b++;
+			if (b >= 10) {
+				b = 0;
+			}
+			a = 0;
+		}
+	}
+	
+	public boolean isValid() {
+		return a >= 0 && a < 10 && b >= 0 && b < 10;
+	}
+	
+	public static void main(String[] args) {
+		BrokenSharing bs = new BrokenSharing();
+		
+		new Thread(new Runnable(){
+			@Override
+			public void run() {
+				while (true) {
+					bs.increment();
+					if (!bs.isValid()) {
+						System.err.println("INVALID!!!!");
+					}
+				}
+			}
+		}).start();
+
+		new Thread(new Runnable(){
+			@Override
+			public void run() {
+				while (true) {
+					if (!bs.isValid()) {
+						System.err.println("INVALID!!!!");
+					}
+				}
+			}
+		}).start();
+	}
+}
---
+++ src/producerconsumer/PipelineSharing.java
@@ -0,0 +1,66 @@
+package producerconsumer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class PipelineSharing {
+	private int a, b;
+
+	public void increment() {
+		a++;
+		if (a >= 10) {
+			b++;
+			if (b >= 10) {
+				b = 0;
+			}
+			a = 0;
+		}
+	}
+
+	public boolean isValid() {
+		return a >= 0 && a < 10 && b >= 0 && b < 10;
+	}
+
+	public static void main(String[] args) {
+		BlockingQueue<PipelineSharing> queue = new ArrayBlockingQueue<>(10);
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int ticker = 0;
+				while (true) {
+					PipelineSharing ps = new PipelineSharing();
+					int target = ThreadLocalRandom.current().nextInt(0, 150);
+					for (int i = 0; i < target; i++) {
+						ps.increment();
+						if (!ps.isValid()) {
+							System.err.println("INVALID!!!!");
+						}
+					}
+					try {
+						queue.put(ps);
+						ps = null;
+					} catch (InterruptedException ie) {
+					}
+					if (++ticker % 100 == 0) {
+						ticker = 0;
+						System.out.print(".");
+						System.out.flush();
+					}
+				}
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						PipelineSharing ps = queue.take();
+					} catch (InterruptedException ie) {}
+				}
+			}
+		}).start();
+	}
+}
